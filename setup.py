8548048ae81a907b7084bb05f96a1f73c75b3b1e
Abner Freitas <defreitasabner@gmail.com>
2024-07-09 11:38:42 -0300

    Changes on package build.

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 from setuptools import setup, find_packages
+import json
 
-def parse_requirements(filename):
+def parse_pipfile_lock(filename, dev = False):
     with open(filename, 'r') as file:
-        return file.read().splitlines()
+        lock_data = json.load(file)
+    dependencies = []
+    dep_type = 'develop' if dev else 'default'
+    for package, details in lock_data[dep_type].items():
+        if 'version' in details:
+            dependencies.append(f"{package}{details['version']}")
+        elif 'git' in details:
+            dependencies.append(f"{package} @ git+{details['git']}@{details['ref']}")
+        elif 'path' in details:
+            dependencies.append(f"{package} @ file://{details['path']}")
+    return dependencies
 
 setup(
     name = 'image_scraper',
     version = 'v0.1.0',
+    author = 'Abner Freitas',
+    author_email = 'defreitasabner@gmail.com',
     packages = find_packages(),
-    install_requires = parse_requirements('requirements.txt'),
+    install_requires = parse_pipfile_lock('Pipfile.lock'),
     extras_require = {
-        'dev': parse_requirements('dev-requirements.txt')
+        'dev': parse_pipfile_lock('Pipfile.lock', dev = True)
     },
     entry_points = {
         'console_scripts': [
